55ccd25d0743b7a86e6d04426f21e65a56114d99
Mystic Dev <75292383+nixxogee@users.noreply.github.com>
2023-02-17 11:24:56 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ import streamlit as st
 
 def local_css(file_name):
     with open(file_name) as f:
-        st.markdown("""<style> .body{background-color: ##FF0000} </style>""", unsafe_allow_html=True)
+        st.markdown("""<style> .ffhzg2{background-color: ##FF0000} </style>""", unsafe_allow_html=True)
 
 
 local_css("style.css")
