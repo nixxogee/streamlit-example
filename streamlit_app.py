82ccd96d25646e6d9ea6cf6d4a14c1d278c97f8a
Mystic Dev <75292383+nixxogee@users.noreply.github.com>
2023-02-17 10:45:44 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,17 @@ import pandas as pd
 import streamlit as st
 
 
-def local_css(site.css):
-    with open(site.css)) as f:
+def local_css(file_name):
+    with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
 
 def remote_css(url):
     st.markdown(f'<link href="{url}" rel="stylesheet">', unsafe_allow_html=True)
+
+
+def icon(icon_name):
+    st.markdown(f'<i class="material-icons">{icon_name}</i>', unsafe_allow_html=True)
 
 
 
